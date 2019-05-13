54c53b026e1d06e1308904c8bc593bd8f990d089
luxury <luxury@luxury.local>
2019-05-12 19:05:57 -0700

    scene_1 working

--- scene_1.py
+++ scene_1.py
@@ -1,20 +1,23 @@
+# from typing import Tuple
+
 
 def CheckPrice(spreadsheet, number):
     num_dict = []
 
     # read spreadsheet and put it in dictionary
-    with open(spreadsheet, 'r') as cost_file:
-            for line in cost_file:
+    with open(spreadsheet, 'r') as f:
+            for line in f:
                 elements = line.rstrip().split(",")
                 num_dict.append((dict(zip(elements[::2], elements[1::2]))))
 
     # write the number taken and the price in output.txt  
-    output_file = open("output.txt", "a") 
+    g = open("output.txt", "a") 
     for i in num_dict:
         for key, value in i.items():
             if key == number:
-                output_file.write("\nNUM: {}   COST: {}".format(key,value))
-                output_file.close
+                g.write("\nNUM: {}   COST: {}".format(key,value))
+                g.close
+                print(value)
             
     
 if __name__ == "__main__":
