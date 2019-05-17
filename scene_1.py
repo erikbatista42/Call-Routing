9e36bd427135b76e8451aa05c413559228a3d84f
luxury <luxury@luxury.local>
2019-05-17 11:51:43 -0700

    + time complexity

--- scene_1.py
+++ scene_1.py
@@ -8,22 +8,23 @@
 *    Availability: https://stackoverflow.com/questions/17714571/creating-a-dictionary-from-a-txt-file-using-python
 *****************************************************
 '''
-
+# Reading file and putting into dictionary => O(n+k+k) -> O(n^3) 
 def CheckPrice(spreadsheet, number):
     num_dict = []
-
+    # O(n) because we are iterating throught each line
     # read spreadsheet and put it in dictionary
     with open(spreadsheet, 'r') as cost_file:
             for line in cost_file:
                 elements = line.rstrip().split(",")
-                num_dict.append((dict(zip(elements[::2], elements[1::2]))))
+                num_dict.append((dict(zip(elements[::2], elements[1::2])))) # O(k+K) k is the slice size. The zip is an O(1) operation.
     
 
+    # BEST + WORST: O(n^2) - because we have two iterations whih both takes n time 
     # write the number taken and the price in output.txt  
     output_file = open("output.txt", "a") 
-    for i in num_dict:
-        for key, value in i.items():
-            if key == number:
+    for i in num_dict: # O(n) - iterate through n dictionaries
+        for key, value in i.items(): # O(n) - have to go through n entries to find
+            if key == number: # O(1)
                 output_file.write("\nNUM: {}   COST: {}".format(key,value))
                 output_file.close
             
--- scene_2.py
+++ scene_2.py
@@ -10,12 +10,13 @@ class CheckPrice():
         self.output_file = output_file
         self.number_and_cost = hashtable.HashTable()
 
+        # O(n + k)
         # read each line in file_to_read and put phone # and cost in a dictionary
         with open(self.file_to_read, "r") as cost_file:
-            for line in cost_file:
-                elements = line.rstrip().split(",")
-                phone_number = elements[::2][0] # key
-                new_cost = elements[1::2][0] # value
+            for line in cost_file: # O(n)
+                elements = line.rstrip().split(",")# O(n + k) k is character sepertator and n in the element
+                phone_number = elements[::2][0] # key O(i1 -i2)
+                new_cost = elements[1::2][0] # value O(i1 -i2)
                 if self.number_and_cost.contains(phone_number):
                     old_cost = self.number_and_cost.get(phone_number)
                     if new_cost < old_cost:
@@ -23,11 +24,12 @@ class CheckPrice():
                 else:
                     self.number_and_cost.set(phone_number, new_cost)
 
-    def price(self):
+    def price(self): 
+        # O(n) - to get the price because we are iterating through n number_and_cost(line)
         # write the number taken and the price in the output file
         with open(self.output_file, "a+") as output_file:
-            for key, value in self.number_and_cost.items():
-                if self.number == key:
+            for key, value in self.number_and_cost.items(): # O(n) because n is the size of the number_and_cost
+                if self.number == key: # O(1) - comparisson is super fast
                     output = "\n{} - ${}".format(key,value)
                     output_file.write(output)
                     return output
