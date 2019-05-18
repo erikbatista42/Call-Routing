1a1066c638d3c688958ee40d5af5a72acc517f5e
luxury <luxury@luxury.local>
2019-05-17 17:55:42 -0700

    finished scenario 2

--- scene_2.py
+++ scene_2.py
@@ -1,46 +1,59 @@
 # hashtable
-import hashtable
+from hashtable import HashTable
+from time import time
 
-class CheckPrice():
+class CheckPrice(object):
 
-    def __init__(self, file_to_read, number, output_file):
+    def __init__(self, file_path):
         # instance variables
-        self.file_to_read = file_to_read
-        self.number = number
-        self.output_file = output_file
-        self.number_and_cost = hashtable.HashTable()
+        self.number_and_cost = self.store_routes_cost(file_path)
 
-        # O(n + k)
-        # read each line in file_to_read and put phone # and cost in a dictionary
-        with open(self.file_to_read, "r") as cost_file:
-            for line in cost_file: # O(n)
-                elements = line.rstrip().split(",")# O(n + k) k is character sepertator and n in the element
-                phone_number = elements[::2][0] # key O(i1 -i2)
-                new_cost = elements[1::2][0] # value O(i1 -i2)
-                if self.number_and_cost.contains(phone_number):
-                    old_cost = self.number_and_cost.get(phone_number)
-                    if new_cost < old_cost:
-                        self.number_and_cost.set(phone_number, new_cost)
+    # store cost of routes into the hash table
+    def store_routes_cost(self, file_path):
+        hash_cost_route = HashTable(100000)  # O(n) space complexity
+        with open(file_path, "r") as file:
+            for line in file:  # O(n) time complexity
+                line = line[:-1]
+                # split the incoming route and cost
+                route_cost = line.split(",")
+                # store the route and cost into it's own variable
+                route = route_cost[0]
+                cost = route_cost[1]
+                # check if the route is already in our hashtable O(1)
+                if hash_cost_route.contains(route):
+                    old_cost = hash_cost_route.get(route)
+                    if old_cost > cost:  # check if the cost already in greater than the new cost coming in
+                        hash_cost_route.set(route, cost)  # O(1)
                 else:
-                    self.number_and_cost.set(phone_number, new_cost)
+                    hash_cost_route.set(route, cost)  # O(1)
+        return hash_cost_route
 
-    def price(self): 
-        # O(n) - to get the price because we are iterating through n number_and_cost(line)
-        # write the number taken and the price in the output file
-        with open(self.output_file, "a+") as output_file:
-            for key, value in self.number_and_cost.items(): # O(n) because n is the size of the number_and_cost
-                if self.number == key: # O(1) - comparisson is super fast
-                    output = "\n{} - ${}".format(key,value)
-                    output_file.write(output)
-                    return output
+    # find cost of the phone number input
+    def find_cost(self, phone_number):
+        for _ in range(len(phone_number)-1):  # O(n) time complexity
+            if self.number_and_cost.contains(phone_number):
+                # O(1) space complexity
+                return self.number_and_cost.get(phone_number)
+            else:
+                phone_number = phone_number[:len(phone_number)-1]
+        return 0
+
+    # write to a new file function
+    def write_results(self, phone_number, cost):
+        with open("call-costs-2.txt", "a") as file:
+            file.write("NUM: {}, COSTS: {}\n".format(phone_number, cost))
 
 
 if __name__ == "__main__":
-    check_price = CheckPrice("costs-10.txt", "+4928843955", "output.txt")
-    print(check_price.price())
-    # table = hashtable.HashTable()
-
-
-
-
-    
+    start_timer = time()
+    print("Working over time, I better get paid...")
+    file_path = "route-costs-106000.txt"  # 10600 routes with costs
+    check_price = CheckPrice(file_path)
+    print("This took {}".format(round(time() - start_timer, 4)))
+    start_second_timer = time()
+    with open("phone-numbers-10000.txt", "r") as file:  # file for the 10000 numbers
+        for phone_number in file:
+            phone_number = phone_number[:-1]
+            cost = check_price.find_cost(phone_number)
+            check_price.write_results(phone_number, cost)
+    print("Finding cost took: {}".format(round(time() - start_second_timer, 4)))
